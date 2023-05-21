169965795edfea05150b3997b857565d857aa438
ethcala <81577564+ethcala@users.noreply.github.com>
2023-05-21 13:05:30 -0600

    links on header

--- cats-cradle/src/components/header.tsx
+++ cats-cradle/src/components/header.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function Header(params:any) {
     return (
         <div className="navbar nav-light">
             <div className="item-holder row">
-                <p className="nav-title">Cat's Cradle</p>
-                <p className="nav-item">Home</p>
-                <p className="nav-item">Search</p>
-                <p className="nav-item">Account</p>
+                <p className="nav-title"><Link href="/index">Cat's Cradle</Link></p>
+                <p className="nav-item"><Link href="/index">Home</Link></p>
+                <p className="nav-item"><Link href="/search">Search</Link></p>
+                <p className="nav-item"><Link href="/account">Account</Link></p>
             </div>
             <a href="javascript:void(0);" className="nav-icon">
                 <i className="fa fa-bars"></i>
