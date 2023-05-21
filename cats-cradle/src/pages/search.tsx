f729137e7ab5797e65924ef7c418c76639201c99
ethcala <81577564+ethcala@users.noreply.github.com>
2023-05-21 12:39:16 -0600

    search start stuff

---
+++ cats-cradle/src/pages/search.tsx
@@ -0,0 +1,20 @@
+export default function Search() {
+    return (
+        <div>
+            <form action="/search-info" method="post">
+                <div className="row">
+                    <input type="radio" id="sellers" name="type" value="sellers" />
+                    <label htmlFor="sellers">Sellers</label>
+                </div>
+                <div className="row">
+                    <input type="radio" id="listings" name="type" value="listings" checked />
+                    <label htmlFor="listings">Listings</label>
+                </div>
+                <div className="row">
+                    <input type="text" name="search" id="search"></input>
+                    <button type="submit">Search</button>
+                </div>
+            </form>
+        </div>
+    )
+}
