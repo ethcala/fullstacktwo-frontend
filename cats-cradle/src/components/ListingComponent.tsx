5b73ef79a0268d6d578a1b8708f0f5c52dc6d489
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2023-05-26 16:34:55 -0600

    Update ListingComponent.tsx

--- cats-cradle/src/components/ListingComponent.tsx
+++ cats-cradle/src/components/ListingComponent.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const ListingComponent = (props:any) => {
   return (
     <div>
-      <h3>{props.listinginfo.listing_name}</h3>
+      <Link href={props.listinginfo.listing_uuid}><h3>{props.listinginfo.listing_name}</h3></Link>
+      
       <h6>${props.listinginfo.listing_price}</h6>
       <p>{props.listinginfo.listing_description}</p>
     </div>
